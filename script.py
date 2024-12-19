a9ae658d89cd6ed8ae3d7b35f09820d4e6ce9bf2
sieep-coding <nstamb6@gmail.com>
2024-12-19 02:41:07 -0500

    scirpt.py

--- script.py
+++ script.py
@@ -31,14 +31,35 @@ def extract_title_and_content(html):
     return title, article_content
 
 def convert_to_markdown(html_content):
-    mardown_converter = html2text.HTML2Text()
-    mardown_converter.ignore_links = False
-    mardown_converter.ignore_images = False
-    return mardown_converter.handle(str(html_content))
+    markdown_converter = html2text.HTML2Text()
+    markdown_converter.ignore_links = False
+    markdown_converter.ignore_images = False
+    return markdown_converter.handle(str(html_content))
 
 def save_file(title, markdown_content):
     safe_title = "".join(c for c in title if c.isalnum() or c in "-_").rstrip()
     filename = f"{safe_title}.md"
     with open(filename, "w", encoding="utf-8") as file:
         file.write(markdown_content)
-    print(f"Article saved successfully")
+    print(f"Article saved successfully as {filename}")
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 script.py <url to Medium article>")
+        sys.exit(1)
+    
+    url = sys.argv[1]
+    print("Fetching...")
+    html = fetch_medium_article(url)
+
+    print("Extracting...")
+    title, article_content = extract_title_and_content(html)
+
+    print("Converting...")
+    markdown_content = convert_to_markdown(article_content)
+
+    print("Saving...")
+    save_file(title, markdown_content)
+
+if __name__ == "__main__":
+    main()
