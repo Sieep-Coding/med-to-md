29f10b03a10eeee95dd89e14c3c5dac79ffbb5aa
sieep-coding <nstamb6@gmail.com>
2024-12-19 10:35:06 -0500

    unit testing

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,5 +1,6 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
+import os
 import utils
 
 class TestUtilsMethods(unittest.TestCase):
@@ -7,5 +8,14 @@ class TestUtilsMethods(unittest.TestCase):
     @patch('utils.subprocess.run')
     @patch('os.makedirs')
     @patch('os.path.exists')
-    def test_run_bash(self, mock_run):
-        mock_run.return
+    def test_run_bash(self, mock_exists, mock_makedirs, mock_run):
+        mock_exists.return_value = False
+        mock_makedirs.return_value = None
+        mock_run.return_value = MagicMock(returncode=0, stderr='', stdout="Success")
+
+        filename = 'test.md'
+
+        utils.Utility.run_bash(filename)
+
+if __name__ == "__main__":
+    unittest.main()
